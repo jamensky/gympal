d15cebc43cdf82044c19f4b73329d795eb4e799c
Jamal <jamensky1991@gmail.com>
2021-05-21 10:07:45 +0100

    finilised log in and log out prcess

--- app/controllers/routines_controller.rb
+++ app/controllers/routines_controller.rb
@@ -1,2 +1,36 @@
 class RoutinesController < ApplicationController
+    def index 
+        @routines = Routines.all
+    end 
+
+    def show
+        @routine = Routine.find_by_id(params[:id]) 
+    end 
+
+    def new 
+        @routine = Routine.new 
+    end 
+
+    def create 
+        
+       if @routine = Routine.new(routine_params)
+          @routine.save
+    
+          redirect_to @routine
+       else 
+          render :new
+       end 
+    end 
+
+    def routine_params
+        params.require(:routine).permit(
+        :name, 
+        :set_1, :set_2, :set_3,
+        :time,
+        :instruction,
+        muscle_groups_ids: [], 
+        muscle_group_attributes: [
+            :name, :muscle_1, :muscle_2, :muscle_3, :muscle_4
+        ])
+    end 
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,6 +10,7 @@ class SessionsController < ApplicationController
             session[:user_id] = @user.id
             redirect_to user_path(@user)
         else 
+            
             redirect_to '/login', :notice => 'Cannot find the user'
         end 
     end 
