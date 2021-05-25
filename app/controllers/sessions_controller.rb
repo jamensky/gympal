b654ebe6a0b36a1846b9dbbd9434aeefd61abc4e
Jamal <jamensky1991@gmail.com>
2021-05-25 22:18:26 +0100

    refactoring various code

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,17 +7,17 @@ class SessionsController < ApplicationController
     end
 
     def create 
-        binding.pry 
         @user = User.find_by(email: params[:email])
+
         if @user
              authenticated = @user.try(:authenticate, params[:password])
               return head(:forbidden) unless authenticated
             session[:user_id] = @user.id
             redirect_to user_path(@user)
-        else 
-            
+        else   
             redirect_to '/login', :notice => 'Cannot find the user detals entered. Please renter details, or register a new user.'
         end 
+        
     end 
 
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,26 +2,28 @@ class UsersController < ApplicationController
     #before_action :require_login 
     #skip_before_action :require_login, only:[:new, :create]
 
-    def show
-        @user = User.find_by_id(session[:user_id]) 
-    end 
-
     def new 
         @user = User.new 
     end 
 
     def create 
-       @user = User.create(user_params)
+        @user = User.create(user_params)
+
        if @user.save
           redirect_to '/login'
        else 
           render :new
        end 
+       
     end 
 
     def edit 
         @user = User.find_by(session[:user_id])
     end
+
+    def show
+        @user = User.find_by_id(session[:user_id]) 
+    end 
 
     def update
         
